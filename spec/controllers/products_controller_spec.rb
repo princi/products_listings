ddcfdea58537a8bec773a04809da5234f1817dbe
peeyush <peeyush@fizzysoftware.com>
2014-08-21 20:25:13 +0530

    moved index render in index

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 describe ProductsController do
 
   describe "GET #index" do
+
     it "responds successfully with an HTTP 200 status code" do
       get :index
       expect(response).to be_success
       expect(response.status).to eq(200)
     end
-  end
 
-  it "renders the index template" do
-    get :index
-    expect(response).to render_template("index")
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
   end
 
   it "redirects to the root path upon save" do
     post :create, product: FactoryGirl.attributes_for(:product)
     response.should redirect_to products_path
   end
-
-  it
 
 end
