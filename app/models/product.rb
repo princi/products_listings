878cd91212471261cf827992f80d74bdfb45ef0a
peeyush <peeyush@fizzysoftware.com>
2014-08-21 15:58:48 +0530

    added product model speccs

---
+++ app/models/product.rb
@@ -0,0 +1,15 @@
+class Product < ActiveRecord::Base
+
+end
+
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  price       :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe Product do
+  it "will create a product with valid values" do
+  end
+end
